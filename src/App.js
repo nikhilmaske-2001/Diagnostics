2119a09acc10f034219b0909237f1d8f51c78d5f
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-11-16 16:23:41 +0530

    Created a basic file structure.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Diagnostics app</div>;
+  return (
+    <div className="App">
+      <div className="tabs"></div>
+    </div>
+  );
 }
 
 export default App;
